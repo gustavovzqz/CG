31c49e7e6b0b3e60ec2698d6c465bad36dddfbb8
gustavovzqz <101258926+gustavovzqz@users.noreply.github.com>
2023-09-19 21:05:57 -0300

    Add files via upload
    Versão correta!

---
+++ task_2/task_2/tela/canvas.cpp
@@ -0,0 +1,14 @@
+#include "canvas.h"
+
+Canvas::Canvas() {}
+
+Canvas::Canvas(int nLines, int nCol, Cor bg) {
+    this->nLines = nLines;
+    this->nCol = nCol;
+    this->bgColor = bg;
+    matriz = new Cor*[nLines];
+    for (int i = 0; i < nLines; i++){
+         matriz[i] = new Cor[nCol];
+    }
+
+}
