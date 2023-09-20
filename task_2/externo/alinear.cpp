375b937a9ec13224d6625fa9999e2616a72d9de9
gustavovzqz <101258926+gustavovzqz@users.noreply.github.com>
2023-09-19 21:16:45 -0300

    Add files via upload

---
+++ task_2/externo/alinear.cpp
@@ -0,0 +1,117 @@
+#include "alinear.h"
+
+// (1) - Definição dos construtores 
+
+Intensidade::Intensidade(){}
+Vetor::Vetor(){}
+Ponto::Ponto(){}
+Cor::Cor(){}
+
+
+Vetor::Vetor(float x, float y, float z){
+    this -> x = x;
+    this -> y = y;
+    this -> z = z;
+}
+
+Intensidade::Intensidade(float r, float g, float b){
+    this -> r = r;
+    this -> g = g;
+    this -> b = b;
+}
+
+Ponto::Ponto(float x, float y, float z){
+    this -> x = x;
+    this -> y = y;
+    this -> z = z;
+}
+
+Cor::Cor(float r, float g, float b){
+    this -> r = r;
+    this -> g = g;
+    this -> b = b;
+}
+
+// (2) - Definições dos métodos "set" para o ponto.
+
+void Ponto::set(float x, float y, float z){
+    this -> x = x;
+    this -> y = y;
+    this -> z = z;
+};
+
+void Ponto::setX(float x){
+    this -> x = x;
+};
+
+void Ponto::setY(float y){
+    this -> y = y;
+};
+
+
+void Ponto::setZ(float z){
+    this -> z = z;
+};
+
+// (3) - Definições de funções úteis
+
+Vetor subP (Ponto p1, Ponto p2){  
+    return Vetor(p1.x - p2.x, p1.y - p2.y, p1.z - p2.z);
+}
+
+Vetor subV (Vetor v1, Vetor v2){ 
+    return Vetor(v1.x - v2.x, v1.y - v2.y, v2.z - v2.z);
+}
+
+Vetor addV (Vetor v1, Vetor v2){ 
+    return Vetor(v1.x + v2.x, v1.y + v2.y, v2.z + v2.z);
+}
+
+Vetor prodVetorC(Vetor v1, float t){
+    return Vetor(v1.x * t, v1.y * t, v1.z * t);
+}
+
+Vetor divVetorC(Vetor v1, float t){
+    return Vetor(v1.x / t, v1.y / t, v1.z / t);
+}
+
+Ponto addPV(Ponto p1, Vetor v1){
+    return Ponto(p1.x + v1.x, p1.y + v1.y, p1.z + v1.z);
+}
+
+float innerProd(Vetor v1, Vetor v2){
+    return ((v1.x * v2.x) + (v1.y * v2.y) + (v1.z * v2.z));
+}
+
+float modulo(Vetor v1){
+    return sqrt(innerProd(v1, v1));
+}
+
+Vetor normalizar(Vetor v1){
+    return divVetorC(v1, modulo(v1));
+}
+
+Intensidade arroba(Intensidade i1, Intensidade i2){
+    return Intensidade(i1.r * i2.r, i1.g * i2.g, i1.b * i2.b);
+}
+
+Intensidade prodIntC(Intensidade i1, float t){
+    return Intensidade(i1.r * t, i1.g * t, i1.b * t);
+}
+
+Intensidade somaI(Intensidade i1, Intensidade i2){
+    return Intensidade(i1.r + i2.r, i1.g + i2.g, i1.b + i2.b);
+}
+
+Cor prodICor(Intensidade i1, Cor c1){
+    return Cor(i1.r * c1.r, i1.g * c1.g, i1.b * c1.b);
+}
+
+
+Intensidade make_eye(Intensidade i1, Intensidade i2, Intensidade i3){
+    return Intensidade(i1.r + i2.r + i3.r,
+                       i1.g + i2.g + i3.g,
+                       i1.b + i2.b + i3.b);
+}
+
+
---
+++ task_2/objetos/esfera.cpp
@@ -0,0 +1,50 @@
+#include "esfera.h"
+#include "math.h"
+
+Esfera::Esfera() {}
+
+Esfera::Esfera(Ponto centro, float raio, Cor cor, Intensidade Ke, Intensidade Ka, Intensidade Kd, float m){
+    this -> cor = cor;
+    this -> raio = raio;
+    this -> centro = centro;
+    this -> Ke = Ke;
+    this -> Ka = Ka;
+    this -> Kd = Kd;
+    this -> m = m;
+}
+
+Cor Esfera::intersecta(Raio r, Luz i, Cor bgColor){
+    Ponto p0 = r.pin;
+    Vetor dr = r.dr;
+    Ponto pint;
+
+
+    Vetor w = subP(p0, centro);
+    float bBarra = innerProd(w, dr);
+    float c = innerProd(w,w) - (raio * raio);
+    float delta = (bBarra * bBarra) - c;
+
+    if (delta < 0)
+        return bgColor; // Se não intersecta, retorna a cor padrão.
+    
+    float t1 = -bBarra - sqrt(delta);
+    float t2 = -bBarra + sqrt(delta);
+
+    Ponto p1 = addPV(p0, prodVetorC(dr, t1));
+    Ponto p2 = addPV(p0, prodVetorC(dr, t2));
+
+    float tam_p1 = modulo(subP(p0, p1));
+    float tam_p2 = modulo(subP(p0, p2));
+        
+    // Encontra o ponto que intersecta PRIMEIRO (em relação ao observador, o mais próximo)
+    if (tam_p1 < tam_p2) 
+        pint = p1;
+    else 
+        pint = p2;
+
+    Vetor n = divVetorC(subP(pint, centro), raio);
+    
+    // Aqui, já temos o ponto de intersecção correto.
+    // Basta chamar a função de iluminação para sabermos a cor correta.
+    return (i.iluminarObjeto(n, pint, dr, Ke, Kd, Ka, m, cor));
+}
