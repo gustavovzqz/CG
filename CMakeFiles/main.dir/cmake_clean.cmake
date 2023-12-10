5e857d881ac02b7dc3d3cd6486c4b321b8b54b8e
Gustavo Vazquez <gustavovzqz.dev@gmail.com>
2023-12-10 17:03:50 -0300

    AGORA ACABOU

---
+++ CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,41 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/cenario/cenario.cpp.o"
+  "CMakeFiles/main.dir/cenario/cenario.cpp.o.d"
+  "CMakeFiles/main.dir/externo/alinear.cpp.o"
+  "CMakeFiles/main.dir/externo/alinear.cpp.o.d"
+  "CMakeFiles/main.dir/externo/luz.cpp.o"
+  "CMakeFiles/main.dir/externo/luz.cpp.o.d"
+  "CMakeFiles/main.dir/externo/renderizar.cpp.o"
+  "CMakeFiles/main.dir/externo/renderizar.cpp.o.d"
+  "CMakeFiles/main.dir/main.cpp.o"
+  "CMakeFiles/main.dir/main.cpp.o.d"
+  "CMakeFiles/main.dir/objetos/cilindro.cpp.o"
+  "CMakeFiles/main.dir/objetos/cilindro.cpp.o.d"
+  "CMakeFiles/main.dir/objetos/cone.cpp.o"
+  "CMakeFiles/main.dir/objetos/cone.cpp.o.d"
+  "CMakeFiles/main.dir/objetos/cubo.cpp.o"
+  "CMakeFiles/main.dir/objetos/cubo.cpp.o.d"
+  "CMakeFiles/main.dir/objetos/esfera.cpp.o"
+  "CMakeFiles/main.dir/objetos/esfera.cpp.o.d"
+  "CMakeFiles/main.dir/objetos/malha.cpp.o"
+  "CMakeFiles/main.dir/objetos/malha.cpp.o.d"
+  "CMakeFiles/main.dir/objetos/plano.cpp.o"
+  "CMakeFiles/main.dir/objetos/plano.cpp.o.d"
+  "CMakeFiles/main.dir/ray/raio.cpp.o"
+  "CMakeFiles/main.dir/ray/raio.cpp.o.d"
+  "CMakeFiles/main.dir/tela/camera.cpp.o"
+  "CMakeFiles/main.dir/tela/camera.cpp.o.d"
+  "CMakeFiles/main.dir/tela/canvas.cpp.o"
+  "CMakeFiles/main.dir/tela/canvas.cpp.o.d"
+  "CMakeFiles/main.dir/tela/janela.cpp.o"
+  "CMakeFiles/main.dir/tela/janela.cpp.o.d"
+  "CMakeFiles/main.dir/tela/textura.cpp.o"
+  "CMakeFiles/main.dir/tela/textura.cpp.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
